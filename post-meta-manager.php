a26dbda85c7c50c52015354f62af97327348e2a0
Pea Lutz <pea@glocal.coop>
2016-06-19 15:45:11 -0400

    Initial commit.

---
+++ post-meta-manager.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Post Meta Data Manager
+ * Plugin URI:      https://github.com/misfist/post-meta-manager
+ * Description:     Plugin to display post meta data in post editor screen.
+ * Author:          Pea
+ * Author URI:      https://github.com/misfist
+ * Text Domain:     post-meta-manager
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Post_Meta_Manager
+ */
